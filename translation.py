181c9cc86f639de72df2a2ec1557a650aa312149
Balaji Shinnosuke <62749324+bluv-pr@users.noreply.github.com>
2021-04-23 20:15:52 +0530

    Add files via upload

---
+++ translation.py
@@ -0,0 +1,44 @@
+class Translation(object):
+    START_TEXT = """ <b> Hi {} , I'm A Exclusive File Renamer+File To Video Converter Bot With Permanent Thumbnail support!💯</b> \n
+
+<b>Bot Maintained By: @TN57_BotZ</b> \n 
+<b> I Can Also Download/Upload Files From Zee5</b> \n
+<b>Do /help for more Details ...</b> \n
+"""
+
+    RENAME_403_ERR = "What Are You Doing? Bruuh!!"
+    BANNED_USER_TEXT = "Sorry!! But @TN57_BotZ Owner As Banned You From Using This Service. That Means You Can't Run Meh Now! \n \n Contact : @BLuVDS For more Details.. " 
+    ABS_TEXT = "What Are You Trying To Do,Mate?"
+    UPGRADE_TEXT = "CONTACT @BLuVDS 💱"
+    DOWNLOAD_START = "<b>Downloading To My server !! Pls Wait</b>"
+    UPLOAD_START = "<b>Downloading Completed Now I'm Uploading Into TG</b>"
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry Do You Think! I'll Upload It?"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me SHARE > ❤️@TN57_BotZ **"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
+    NOT_AUTH_USER_TEXT = "CONTACT @BLuVDS "
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @BLuVDS "
+    SAVED_CUSTOM_THUMB_NAIL = "<b>thumbnail Saved ✅ This Is Permanent Until</b> /delthumb ❤"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "thumbnail cleared succesfully🤕"
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
+    SAVED_RECVD_DOC_FILE = "<b>File Downloaded Successfully 😎</b>"
+    REPLY_TO_DOC_FOR_RENAME_FILE = "<b>Please Reply To An File With /rename Text .extension</b>"
+    REPLY_TO_DOC_FOR_C2V = "<b> Please Reply To An File With /c2v To Convert It Into Streamable video File</b>"
+    REPLY_TO_DOC_FOR_C2A = "<b> Please Reply To An File/video With /c2a To Convert It Into Audio File</b>"
+    CUSTOM_CAPTION_UL_FILE = " "
+    NO_THUMB_FOUND = "No Thumbnail found"
+    FORMAT_SELECTION = """<b>Choose appropriate option</b> <a href='{}'>⬇️</a>
+🎞  - Stream format
+📁  - File format
+<b>NOTE : Taking high resolutions may result in files above 2GB and hence cannot Upload to TG. So better select a medium resolution.</b> 😇
+"""
+    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+    IFLONG_FILE_NAME = """You Gotta Be Kidding Me...Decrease The Number Of Letters😆😉"""
+    ABOUT_ME = """<b>Bruh : \n Im Telegram File Renamer Bot Which Can Rename A Telegram Files. \n You Can Set Permanent Thumbnail For The File So You Don't Have To Send Custom Thumbs All The Time.Me \n \n i can also convert files into videos \n Feedback @BLuVDS If You Found Me Useful</b>"""
+    HELP_USER = """Ohh You Want Help?? 😅
+    
+1.🔹 <b>Send me any Telegram File.</b> \n
+2.🔹 <b>Reply to that message with /rename New file name.extension</b> \n
+3.🔹 <b> Send A Photo to make it as permanent Thumbnail</b> \n
+4.🔹 <b> I Can Also Convert Files Into Video through /c2v Command </b>   \n
+5.🔹 <b> Send Me Any Zee5 Url And I'll Upload It Into Telegram </b> \n \n <b>Made With Love By: @TN57_BotZ </b> \n <b>Support Group : @TN57_BotzSupport </b>"""
+
