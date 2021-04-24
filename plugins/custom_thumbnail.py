66557121b51535a46ac4653d14eed9bbe017c36b
Balaji Shinnosuke <62749324+bluv-pr@users.noreply.github.com>
2021-04-24 23:30:44 +0530

    Update custom_thumbnail.py

--- plugins/custom_thumbnail.py
+++ plugins/custom_thumbnail.py
@@ -23,11 +23,11 @@ from database.database import *
 @Client.on_message(filters.photo)
 async def save_photo(bot, update):
     if update.from_user.id in Config.BANNED_USERS:
-        <b>Sorry You Are B a n n e d...!!! \n \nContact My Dev 👉 @BluVds To Resolve This Problem</b")
+        <b>Sorry You Are B a n n e d...!!! \n \nContact My Dev 👉 @BluVds To Resolve This Problem</b>")
                return
         except UserNotParticipant:
             await update.reply_text(
-                text="**<b>Oh Dear In Order To Use Me Join My Update Channel 🤭</b**",
+                text="**<b>Oh Dear In Order To Use Me Join My Update Channel 🤭</b>**",
                 reply_markup=InlineKeyboardMarkup([
                     [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/TN57_BotZ")]
               ])
